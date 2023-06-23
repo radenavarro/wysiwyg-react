c3f03259284c0c9e5baa796a4c7832dea9330411
Radenavarro <r.paz@orca.es>
2023-06-23 17:10:09 +0200

    hook useEmoteFolder devuelve las imagenes bien

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useMemo, useRef, useState} from "react";
+import React, {useCallback, useEffect, useMemo, useRef, useState, lazy} from "react";
 import "./Textarea.scss";
 import {emojisW10} from "../fallback/emotes/emojisW10";
 import {useEmoteFolder} from "../customHooks/customHooks";
@@ -9,7 +9,8 @@ const Textarea = (
     value = "",
     rows = 20,
     cols = 80,
-    onTextChange
+    onTextChange,
+    ...props
   }
 ) => {
   const [parsedValue, setParsedValue] = useState(value);
@@ -30,17 +31,17 @@ const Textarea = (
   useEffect(() => {
     setParsedValue(value)
   }, [value])
-
-  const replaceByEmotes = useCallback(debounce((txt) => {
-    if (emotes?.images && emotes?.images?.length > 0) {
-      emotes.images.forEach(async(emote) => {
-        console.log(emote);
+  console.log(emotes)
+  const replaceByEmotes = useCallback(debounce(async(txt) => {
+    // if (emotes.images && emotes.images?.length > 0) {
+      console.log(emotes.images)
+      emotes.images?.forEach((emote) => {
         txt = txt.replaceAll(Object.keys(emote)?.[0] ?? "", <img src={emote.src}/>)
       })
-    }
-    Object.entries(emojisW10).forEach((entry) => {
-      txt = txt.replaceAll(entry[0], entry[1])
-    })
+    // }
+    // Object.entries(emojisW10).forEach((entry) => {
+    //   txt = txt.replaceAll(entry[0], entry[1])
+    // })
     // console.log(txt)
     setParsedValue(txt);
   }, 1000),[])
--- src/customHooks/customHooks.js
+++ src/customHooks/customHooks.js
@@ -1,39 +1,51 @@
 import {useEffect, useState} from "react";
 
 export const useEmoteFolder = () => {
-  const [imageSet, setImageSet] = useState({images:[]});
-
-  function mapKeys(r) {
-    return r.keys().map(r) ?? [];
-  }
-
-  async function loadSrcs(mappedKeys) {
-    let loadedSrcs = [];
-    for await (let emote of mappedKeys) {
-      let imgname = emote.match(/([^(.)]*)/gm)?.[0]?.replace("/static/media/", "");
-      let extension = emote.split(".")?.pop();
-      let src = await import(`../img/emotes/${imgname}.${extension}`);
-      loadedSrcs.push ({
-        [imgname]: {
-          fullname: emote,
-          src
-        }
-      })
-    }
-    return loadedSrcs;
-  }
-
-  async function importAll(r) {
-    let mappedKeys = mapKeys(r);
-    if (mappedKeys && mappedKeys?.length > 0) {
-      mappedKeys = await loadSrcs(mappedKeys);
-    }
-    setImageSet({images: mappedKeys});
-  }
+  const [imageData, setImageData] = useState([]);
 
   useEffect(() => {
-    importAll(require.context('../img/emotes', true, /\.(png|jpe?g|svg)$/))
-  },[])
+    const loadImageData = async () => {
+      try {
+        const context = require.context('../img/emotes', true, /\.(png|jpe?g|gif|svg|webp)$/);
+        const fileNames = context.keys();
+        const loadedImages = [];
 
-  return imageSet;
-}
+        for (const fileName of fileNames) {
+          const imageObject = {
+            fullname: undefined,
+            src: undefined
+          }
+          // console.log(fileName);
+          let matches = context(fileName).match(/^\/static\/media\/(.+)\.[a-zA-Z0-9]+(\.\w+)$/);
+          // console.log(matches)
+          if (matches && matches.length === 3) {
+            const fileName = matches[1];
+            const fileExtension = matches[2];
+
+            // const imageUrl = `../img/emotes/${fileName}${fileExtension}`;
+            console.log(`../img/emotes/${fileName}${fileExtension}`)
+            const imageUrl = await import(`../img/emotes/${fileName}${fileExtension}`);
+
+            imageObject.fullname = fileName.replace('./', '');
+            imageObject.src = imageUrl.default
+
+          }
+
+
+          loadedImages.push(imageObject);
+        }
+
+        setImageData(loadedImages);
+      } catch (error) {
+        console.error('Error al cargar las imágenes:', error);
+      }
+    };
+
+    loadImageData();
+
+  }, []);
+
+  return {
+    images: imageData
+  };
+};
